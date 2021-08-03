f5f2575e1f255ce8df0d117d84d92293cf57ae03
Prabhu Chaitanya Areti <pareti@vsoftconsulting.com>
2021-08-03 12:47:11 +0530

    initial commit

--- app/src/main/java/com/vsoft/goodmankotlin/DashBoardActivity.kt
+++ app/src/main/java/com/vsoft/goodmankotlin/DashBoardActivity.kt
@@ -1,10 +1,16 @@
 package com.vsoft.goodmankotlin
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 class DashBoardActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var addOperator: TextView
@@ -36,7 +42,12 @@ class DashBoardActivity : AppCompatActivity(), View.OnClickListener {
 
         }
         if(v?.id==sync.id){
-
+            var jsonObject= JsonObject()
+            val gson = Gson()
+            jsonObject.addProperty("Die Id","Die Id")
+            jsonObject.addProperty("Part Id","Part Id")
+            jsonObject.addProperty("file_name","check1.mp4")
+            save(this,gson.toJson(jsonObject))
         }
         if(v?.id==skip.id){
             navigateToOperatorSelection()
@@ -46,5 +57,15 @@ class DashBoardActivity : AppCompatActivity(), View.OnClickListener {
         val mainIntent = Intent(this, OperatorSelectActivity::class.java)
         startActivity(mainIntent)
         finish()
+    }
+
+    @Throws(IOException::class)
+    fun save(context: Context, jsonString: String?) {
+        val rootFolder: File? = context.getExternalFilesDir(null)
+        val jsonFile = File(rootFolder, "details.json")
+//        val writer = FileWriter(jsonFile)
+//        writer.write(jsonString)
+//        writer.close()
+        //or IOUtils.closeQuietly(writer);
     }
 }
